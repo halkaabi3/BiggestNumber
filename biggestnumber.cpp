91538175b37ec456c433b710ac875d0f84e65889
halkaabi3 <halkaabi3@mail.csuchico.edu>
2018-02-06 14:22:30 -0800

    Finds the giggest number

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,13 +5,34 @@ using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
+
+int a;
+
+int b;
+
+int biggest;
+
+
+
+
   cout<<"Please enter a whole number:\n";
-  
-  cout<<"Please enter another whole number:\n";
 
-  cout<<"Of those two numbers, the biggest is: ";
-  cout<<endl<<"Thank you for playing.\n";
+  cin>>a;
 
-  return 0;
-}
+    cout<<"Please enter another whole number:\n";
+
+    cin>>b;
+
+    if(a>b){biggest=a;}
+    else {biggest=b;}
+
+
+
+    cout<<"Of those two numbers, the biggest is: "<< biggest <<endl;
+
+    cout<<"Thank you for playing."<<endl;;
+
+
+
+      return 0;
+      }
